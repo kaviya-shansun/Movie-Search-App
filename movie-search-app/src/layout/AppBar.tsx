0e7d6afd52c6fb84de1a557d30f1c1c143cf9b59
kaviya-shansun <kaviya.shanmuga15082001@gmail.com>
2026-01-14 21:19:07 +0530

    Added drawerLeft and made UI update

---
+++ movie-search-app/src/layout/AppBar.tsx
@@ -0,0 +1,35 @@
+import { styled } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import type { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import "./DrawerLeft.css";
+
+const drawerWidth = 220;
+
+interface CustomAppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<CustomAppBarProps>(({ theme, open }) => ({
+  backgroundColor: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  boxShadow: theme.shadows[1],
+
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+
+export default AppBar;
---
+++ movie-search-app/src/layout/MainBar.tsx
@@ -0,0 +1,25 @@
+import { styled } from "@mui/material/styles";
+import "./DrawerLeft.css";
+
+const drawerWidth = 220;
+
+const MainBar = styled("main", {
+  shouldForwardProp: (prop) => prop !== "open",
+})<{ open?: boolean }>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  marginLeft: `-${drawerWidth}px`,
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: 0,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default MainBar;
